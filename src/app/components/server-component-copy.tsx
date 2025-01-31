5008b101b5c243329f1d07a4316b6d73262fc8c9
YevheniiaYakymenko <zh.yakymenko@gmail.com>
2025-01-31 22:15:29 +0100

    use Server and Client Components together

---
+++ src/app/components/server-component-copy.tsx
@@ -0,0 +1,19 @@
+// import React from 'react';
+// import { headers } from 'next/headers';
+//
+// export interface ServerComponentProps {
+//   children?: React.ReactNode;
+// }
+//
+// export default function ServerComponentCopy({
+//   children,
+// }: ServerComponentProps) {
+//   console.log('ServerComponent Copy');
+//   console.log(headers());
+//   return (
+//     <div>
+//       <span>Server Component Copy</span>
+//       {children}
+//     </div>
+//   );
+// }
